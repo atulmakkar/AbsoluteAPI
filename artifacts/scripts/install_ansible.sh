982b991e18d27ca143f5d50344b96e6b5c516d48
atulmakkar <atulmakkar@users.noreply.github.com>
2019-03-11 10:51:47 -0700

    Initial Commit

---
+++ artifacts/scripts/install_ansible.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Install Ansible repository.
+apt -y update && apt-get -y upgrade
+apt-get install software-properties-common
+apt-add-repository ppa:ansible/ansible -y
+
+# Install Ansible.
+apt-get update
+apt-get install ansible -y
+
+# Install expect, dos2unix & tree
+apt-get install expect -y 
+apt-get install dos2unix -y
+apt-get install tree -y 
+
+# Installing unzip
+# apt-get install unzip -y 
+
+# Add vagrant user to sudoers.
+echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+# generating password configuration on ansible server to access remote servers
+echo vagrant | sudo -S su - vagrant -c "ssh-keygen -t rsa -f /home/vagrant/.ssh/id_rsa -q -P ''"
