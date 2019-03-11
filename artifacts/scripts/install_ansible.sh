30e9e6fbeace252688dc4bdb7914e107893bf5e1
atulmakkar <atulmakkar@users.noreply.github.com>
2019-03-11 11:42:03 -0700

    Adding line to sudoers

--- artifacts/scripts/install_ansible.sh
+++ artifacts/scripts/install_ansible.sh
@@ -14,12 +14,8 @@ apt-get install expect -y
 apt-get install dos2unix -y
 apt-get install tree -y 
 
-# Installing unzip
-# apt-get install unzip -y 
-
 # Add vagrant user to sudoers.
 echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
 # generating password configuration on ansible server to access remote servers
 echo vagrant | sudo -S su - vagrant -c "ssh-keygen -t rsa -f /home/vagrant/.ssh/id_rsa -q -P ''"
